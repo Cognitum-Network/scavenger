a1ad587e0829617fecc54e795bfa77419026074a
Ahmad Sofyan <asofyan@gmail.com>
2023-01-23 04:03:58 +0700

    map commit

--- ts-client/scavenge.scavenge/registry.ts
+++ ts-client/scavenge.scavenge/registry.ts
@@ -1,11 +1,11 @@
 import { GeneratedType } from "@cosmjs/proto-signing";
-import { MsgRevealSolution } from "./types/scavenge/scavenge/tx";
 import { MsgSubmitScavenge } from "./types/scavenge/scavenge/tx";
+import { MsgRevealSolution } from "./types/scavenge/scavenge/tx";
 import { MsgCommitSolution } from "./types/scavenge/scavenge/tx";
 
 const msgTypes: Array<[string, GeneratedType]>  = [
-    ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
     ["/scavenge.scavenge.MsgSubmitScavenge", MsgSubmitScavenge],
+    ["/scavenge.scavenge.MsgRevealSolution", MsgRevealSolution],
     ["/scavenge.scavenge.MsgCommitSolution", MsgCommitSolution],
     
 ];
